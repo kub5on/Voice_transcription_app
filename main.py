05b940d7e3de80b8782015cc07e74368a293d95c
kub5on <jaakub.pankowski@gmail.com>
2025-08-25 18:40:35 +0200

    voice_transcription function created

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import speech_recognition as sr
-
-r = sr.Recognizer()
-mic = sr.Microphone()
+import os
 
 
+def voice_transcription():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    all = []
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("Start talking: ")
+        try:
+            while True:
+                audio = r.listen(source)
+
+                try:
+                    res = r.recognize_google(audio, language="pl-PL")
+                    all.append(res)
+                    print("You said" + res)
+                except sr.UnknownValueError:
+                    print("Google Speech Recognition could not understand audio")
+                except sr.RequestError as e:
+                    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        except KeyboardInterrupt:
+            print("Recording stopped.")
+
+    return "\n".join(all)
+
+
+if __name__ == "__main__":
+    result = voice_transcription()
+
+with open('wynik.txt', mode='w', encoding='UTF-8') as f:
+    f.write(result)
 
 
 
